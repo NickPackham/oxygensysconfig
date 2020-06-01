d8af2d3affc10470def3e7b53477cd094710702a
Nick Packham <46549847+NickPackham@users.noreply.github.com>
2020-05-31 19:09:47 -0700

    Create config.lua

---
+++ lua/voy_oxygen_config/config.lua
@@ -0,0 +1,11 @@
+VOY_Oxygen_Config = {}
+
+VOY_Oxygen_Config_MaxOxygen = 100 -- Max Oxygen amount for all players besides for those in the teams.
+
+VOY_Oxygen_Config_DrownAmount = 0 -- At what Oxygen level should drowning sounds start playing?
+
+VOY_Oxygen_Config_Hud = true -- Put false if you don't want the HUD or want to make your own.
+
+VOY_Oxygen_NoLossTeam = { -- What teams shouldn't lose Oxygen?
+	["Gun Dealer"] = false,
+}
